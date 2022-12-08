0e9026b5285cbc693e0263eea1f514c5641da41e
iliacodes <6ac177ed@protonmail.com>
2022-12-08 15:05:52 -0800

    created a template for the html document to append to tweets. Used prepend to add in chronological order.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,64 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$( document ).ready(function (){
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  // const $tweet = createTweetElement(tweetData);
+  
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $(".tweets-container").prepend(createTweetElement(tweet));
+    }
+  }
+  
+  const createTweetElement = function(tweet) {
+    return $(`<article class="tweet">
+    <header>
+      <div>
+        <img src=${tweet.user.avatars} width="75px">
+        <p>${tweet.user.name}</p>
+      </div>
+      <p>${tweet.user.handle}</p>
+    </header>
+    <p class="tweet-content">${(tweet.content.text)}</p>
+    <footer>
+      <p>2 days ago</p>
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`);
+  }
+
+  $.ajax('')
+  
+  renderTweets(data);
+})
 
