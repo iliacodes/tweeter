0e890ed154aef905253e3bd7e6dfa226bf1fc251
iliacodes <6ac177ed@protonmail.com>
2022-12-08 16:11:09 -0800

    completed form submission for tweet using jquery.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,41 +3,41 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$( document ).ready(function (){
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
+$(document).ready(function() {
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+
   // const $tweet = createTweetElement(tweetData);
-  
-  
+
+
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       $(".tweets-container").prepend(createTweetElement(tweet));
     }
-  }
-  
+  };
+
   const createTweetElement = function(tweet) {
     return $(`<article class="tweet">
     <header>
@@ -57,10 +57,31 @@ $( document ).ready(function (){
       </div>
     </footer>
   </article>`);
-  }
+  };
 
-  $.ajax('')
-  
+
+  $('#tweet-form').on('submit', function(event) {
+    console.log('Submitting the form.');
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    console.log(data);
+    $.post('/tweets', data);
+
+
+
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {
+      method: 'GET',
+      dataType: 'JSON'
+    }).then(function(render) {
+      renderTweets(render);
+    });
+  };
+
+
   renderTweets(data);
 })
 
