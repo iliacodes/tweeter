4093b5494595a8f1899ddb76cfcf1783c450d8e6
iliacodes <6ac177ed@protonmail.com>
2022-12-08 16:51:21 -0800

    created a function to render tweets, implemented time ago into function template.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
   // const $tweet = createTweetElement(tweetData);
-
 
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
@@ -49,7 +48,7 @@ $(document).ready(function() {
     </header>
     <p class="tweet-content">${(tweet.content.text)}</p>
     <footer>
-      <p>2 days ago</p>
+      <p>${timeago.format(tweet.created_at)}</p>
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -68,7 +67,7 @@ $(document).ready(function() {
     console.log(data);
     $.post('/tweets', data);
 
-
+    
 
   });
 
@@ -81,7 +80,7 @@ $(document).ready(function() {
     });
   };
 
-
-  renderTweets(data);
+  loadTweets();
+  // renderTweets(data);
 })
 
